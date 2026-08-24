44c5ff98289a58a61751a48c176636c8c9a8856b
VViller <vviller959@gmail.com>
2026-08-24 21:17:17 +0500

    24082026_1

---
+++ js/Урок.js
@@ -0,0 +1,17 @@
+"use strict";
+const PI = 3.14;
+let a = "" + 3 + 1;
+alert(a);
+//
+
+let str = true;
+let str1 = false;
+let str2;
+
+console.log("123" * "25");
+console.log(123 + PI);
+console.log(10 / 0);
+console.log(NaN);
+console.log(str);
+console.log(str1);
+console.log(str2);
